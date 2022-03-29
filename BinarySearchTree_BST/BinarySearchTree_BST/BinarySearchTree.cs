829622fd9334727bbc85904eb2fd165998f1ba2a
unknown <kaithwas.697@gmail.com>
2022-03-29 16:35:08 +0530

    Created Binary search tree

---
+++ BinarySearchTree_BST/BinarySearchTree_BST/BinarySearchTree.cs
@@ -0,0 +1,55 @@
+﻿using System;
+namespace BinarySearchTree_BST
+{
+    internal class BinarySearchTree<T> where T : IComparable<T>
+    {
+        public T nodeData;
+        public BinarySearchTree<T> left;
+        public BinarySearchTree<T> right;
+        public BinarySearchTree(T data)
+        {
+            nodeData = data;
+            left = null;
+            right = null;
+        }
+
+        public void Insert(T item)
+        {
+            T node = nodeData;
+            if (node.CompareTo(item) > 0)
+            {
+                if (left == null)
+                {
+                    left = new BinarySearchTree<T>(item);
+                }
+                else
+                {
+                    left.Insert(item);
+                }
+            }
+            else
+            {
+                if (right == null)
+                {
+                    right = new BinarySearchTree<T>(item);
+                }
+                else
+                {
+                    right.Insert(item);
+                }
+            }
+        }
+        public void Display()
+        {
+            if (left != null)
+            {
+                left.Display();
+            }
+            Console.WriteLine(nodeData.ToString());
+            if (right != null)
+            {
+                right.Display();
+            }
+        }
+    }
+}
---
+++ BinarySearchTree_BST/BinarySearchTree_BST/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace BinarySearchTree_BST
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BinarySearchTree<int> binarySearchTree = new BinarySearchTree<int>(56);
+            binarySearchTree.Insert(30);
+            binarySearchTree.Insert(70);
+            binarySearchTree.Display();
+        }
+    }
+}
